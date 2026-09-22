627691b2d9fb7b71d0fa8af51dec8aa8f279941c
Arlo-Fuls <arlofuls@gmail.com>
2026-09-22 15:36:56 +0200

    Range Guide Start

--- src/pages/Location.jsx
+++ src/pages/Location.jsx
@@ -1,7 +1,35 @@
+import { Link } from "react-router";
+import Navbar from "../components/Navbar.jsx";
 import "./Location.scss";
 
 function LocationPage() {
-  return <></>;
+  return (
+    <>
+      <header className="rangerGuide__mainHeader mainSection">
+        <div className="header__background"></div>
+        {/* Page specific navbar */}
+        <div className="navMain__container--location">
+          <Navbar />
+        </div>
+
+        <div className="header__bottom clamped">
+          <div className="header__left">
+            <h2>Range Guide</h2>
+            <p>Take a look at the path to the range with all of the most important stops on the way</p>
+          </div>
+          <button className="location__nextButton">Next</button>
+        </div>
+        <h2></h2>
+      </header>
+
+      <main>
+        <div className="locationImage__container"></div>
+        <section className="rangeGuide--1 mainSection">
+          <h3>Getting to the range</h3>
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default LocationPage;
